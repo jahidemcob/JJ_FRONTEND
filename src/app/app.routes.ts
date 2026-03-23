ab0a487ca6ddfc3f23560db6989632c8fe58e5aa
Jahidem Cobos <jicobos@ucundinamarca.edu.co>
2026-03-23 18:21:12 -0500

    Desarrollo estructura y prototipo inicial login y registro

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './modules/auth/pages/login/login';
+import { RegisterComponent } from './modules/auth/pages/register/register';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
