9f063d8b533ae784c5f298d3b2b8e7ce0255a1c2
Jahidem Cobos <jicobos@ucundinamarca.edu.co>
2026-04-08 11:56:27 -0500

    interfaaz visual lista usuarios 100% terminada

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -9,7 +9,7 @@ export const authGuard: CanActivateFn = (route) => {
 
   // 🔐 Validar sesión
   if (!authService.isLoggedIn()) {
-    return router.createUrlTree(['/login']);
+    return true;
   }
 
   // 🎯 Rol desde JWT
